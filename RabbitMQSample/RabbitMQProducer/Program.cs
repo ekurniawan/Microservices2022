47fddc4ff8c709305e83627418192b8673201326
ekurniawan <erick.kurniawan@gmail.com>
2022-04-21 13:58:03 +0700

    menambahkan multiple data

--- RabbitMQSample/RabbitMQProducer/Program.cs
+++ RabbitMQSample/RabbitMQProducer/Program.cs
@@ -13,7 +13,15 @@ using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 channel.QueueDeclare("demo-queue",durable:true,
     exclusive:false,autoDelete:false,arguments:null);
-var message = new {Name="Producer",Message="Hello !"};
-var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
-channel.BasicPublish("","demo-queue",null,body);
+
+var count = 0;
+while(true)
+{
+    var message = new {Name="Producer",Message=$"Hello Count: {count}"};
+    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+    channel.BasicPublish("","demo-queue",null,body);
+    count++;
+    Thread.Sleep(1000);
+    Console.WriteLine($"Mengirimkan pesan count {count}");
+}
 
