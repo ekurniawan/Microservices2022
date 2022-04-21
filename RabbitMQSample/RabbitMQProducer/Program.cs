ec57f0969507203706b57420d154bceda77e7431
ekurniawan <erick.kurniawan@gmail.com>
2022-04-21 12:05:46 +0700

    menambahkan latihan rabbitmq

---
+++ RabbitMQSample/RabbitMQProducer/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
