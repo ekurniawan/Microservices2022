026d08fedf4fa8ac25bfbdb0f164c9ae25fa9e18
ekurniawan <erick.kurniawan@gmail.com>
2022-04-21 15:06:46 +0700

    menambahkan send message to rabbitmq on platform services

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
 
@@ -27,6 +28,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
+builder.Services.AddSingleton<IMessageBusClient,MessageBusClient>();
 builder.Services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>();
 
 var app = builder.Build();
